21d5a149ecac4d3c3ba9a49ec70604fa95d6a77f
lestafo <santiagolestaf@gmail.com>
2020-04-26 00:31:15 -0300

    commit inicial

---
+++ app/src/main/java/com/example/exampleapp/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.exampleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.exampleapp.MESSAGE";
+    private ViewHolder nViewHolder = new ViewHolder();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        this.nViewHolder.editValue = findViewById(R.id.editText);
+        this.nViewHolder.butao = findViewById(R.id.button);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /*this.nViewHolder.butao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                code
+            }
+        });*/ //or that for the buton
+        //or even
+        /*this.nViewHolder.butao.setOnClickListener(this);, and the mainActivity implementing View.OnClickListener*/
+    }
+    private static class ViewHolder{
+        EditText editValue;
+        Button butao;
+    }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = nViewHolder.editValue;
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+
+    }
+
+}
