e0a78c6fb3c202613583f3e33360d2f87ca45493
lestafo <santiagolestaf@gmail.com>
2020-04-29 00:45:52 -0300

    commit inicial

--- app/src/main/java/com/example/deiteltwitter/MainActivity.java
+++ app/src/main/java/com/example/deiteltwitter/MainActivity.java
@@ -2,9 +2,10 @@ package com.example.deiteltwitter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ListActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
